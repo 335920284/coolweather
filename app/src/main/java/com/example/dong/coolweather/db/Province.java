9a4ed296a5ed7893d2b740a69dfa824d48dff69f
snake <335920284.com>
2018-03-05 17:33:44 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/dong/coolweather/db/Province.java
@@ -0,0 +1,34 @@
+package com.example.dong.coolweather.db;
+
+import DataSupport;
+
+/**
+ * Created by dong on 2018/3/5.
+ */
+
+public class Province extends DataSupport{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
